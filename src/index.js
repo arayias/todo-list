7e95d03d5fdbaa40268fc1349043dad3a17c98e2
Andreas Rayias <wowcaeu.andreas@gmail.com>
2023-08-28 01:00:54 +0300

    Added an example layout for the project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 import "./style.css";
 
-console.log("Hello World");
+function header() {
+  const body = document.querySelector("body");
+  const element = document.createElement("header");
+  element.innerHTML = "TodaLoo";
+  body.appendChild(element);
+}
+
+function mainGrid() {
+  const body = document.querySelector("body");
+  const element = document.createElement("div");
+  element.classList.add("main-grid");
+  body.appendChild(element);
+
+  element.appendChild(sidebar());
+  element.appendChild(content());
+}
+
+function sidebar() {
+  const element = document.createElement("div");
+  element.classList.add("sidebar");
+  element.innerHTML = "Sidebar";
+  return element;
+}
+
+function content() {
+  const element = document.createElement("div");
+  element.classList.add("content");
+  element.innerHTML = "Content";
+  return element;
+}
+
+header();
+mainGrid();
